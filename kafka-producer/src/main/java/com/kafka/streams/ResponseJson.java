1e6d4269694e0cf13e4bdb36dcc960fdf0687e4c
amdocsyakkin <yakup.akkin@vodafone.com>
2022-08-03 08:37:43 +0300

    responsejson

--- kafka-producer/src/main/java/com/kafka/streams/ResponseJson.java
+++ kafka-producer/src/main/java/com/kafka/streams/ResponseJson.java
@@ -3,41 +3,15 @@ package com.kafka.streams;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import lombok.Builder;
+import lombok.Data;
 
-@Builder
+@Data
 @JsonPropertyOrder({ "id", "message" })
 public class ResponseJson {
-	@Builder.Default
 	@JsonProperty("id")
 	private Integer id;
-	@Builder.Default
 	@JsonProperty("message")
 	private String message;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public ResponseJson(Integer id, String message) {
-		this.id = id;
-		this.message = message;
-	}
-
-	public ResponseJson() {
-
-	}
 }
 
